68bef7c08ed67d3135b2b54f2818bf719f1a7217
AlexGundrum <alex_gundrum@yahoo.com>
2025-09-27 01:08:32 -0400

    important

--- backend/utils/llm_communication.py
+++ backend/utils/llm_communication.py
@@ -3,15 +3,15 @@ from fastapi import FastAPI
 
 import os
 import google.generativeai as genai
+class llm_communication:
+    genai.api_key = os.getenv("GOOGLE_API_KEY")
+    our_model = "gemini-2.5-flash"
 
-genai.api_key = os.getenv("GOOGLE_API_KEY")
-our_model = "gemini-2.5-flash"
-
-def ask_gemini(prompt: str, model: str = our_model) -> str:         # WILL NEED ANOTHER FUNCTION THAT GENERATES THE PROMPT THAT WE PASS HERE
-   
-    response = genai.models.generate_content(
-        model=model,
-        contents=prompt
-    )
-    return {"response" , response.text }
+    def ask_gemini(prompt: str, model: str = our_model) -> str:         # WILL NEED ANOTHER FUNCTION THAT GENERATES THE PROMPT THAT WE PASS HERE
+    
+        response = genai.models.generate_content(
+            model=model,
+            contents=prompt
+        )
+        return {"response" , response.text }
 
